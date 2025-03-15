48ce2f88c2d947bfb614e801483c8dcecf207777
nadya-qo <nadya.gorch@mail.ru>
2025-03-15 21:30:14 +0400

    Добавлен чекбокс для добавления или удаления параграфа в начале результата

--- contents.js
+++ contents.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const inputWindow = document.querySelector('.input__window');
     const eductionWindow = document.querySelector('.eduction__window');
+    const checkP = document.getElementById('check-title');
 
-    if (!inputWindow || !eductionWindow) {
-        console.error("элементы не найдены в DOM");
+    if (!inputWindow || !eductionWindow || !checkP) {
+        console.error("Один из элементов не найден в DOM");
         return;
     }
 
     inputWindow.addEventListener('input', function () {
+        generateTableOfContents(inputWindow.value);
+    });
+
+    checkP.addEventListener('change', function () { 
         generateTableOfContents(inputWindow.value);
     });
 
@@ -16,7 +21,6 @@ document.addEventListener('DOMContentLoaded', function () {
         const tableOfContents = document.createElement('ul');
 
         headings.forEach(function (heading, index) {
-
             const normalizedHeading = heading.replace(/&nbsp;/g, ' ');
             const id = generateId(normalizedHeading, index);
             const listItem = document.createElement('li');
@@ -30,10 +34,13 @@ document.addEventListener('DOMContentLoaded', function () {
             tableOfContents.innerHTML += `\n<li>${link.outerHTML}</li>\n`;
 
             // добавляется id
-            text = text.replace(heading, `<h${heading.charAt(2)} id="${id}">${normalizedHeading.replace(/<\/?[^>]+(>|$)/g, '')}</h${heading.charAt(2)}>`);
+            text = text.replace(heading, `<h${heading.charAt(2)} id="${id}">${normalizedHeading.replace(/<\/?[^>]+(>|$)/g, '')}</h${heading.charAt(2)}>`); 
         });
 
-        eductionWindow.value = '<p><strong>Оглавление:</strong></p>' + '\n\n' + tableOfContents.outerHTML + '\n\n' + text;
+        // Проверка - включен ли чекбокс для параграфа "Оглавление"
+        const pContents = checkP.checked ? '<p><strong>Оглавление:</strong></p>\n\n' : '';
+
+        eductionWindow.value = pContents + tableOfContents.outerHTML + '\n\n' + text;
     }
 
     function generateId(heading, index) {
@@ -52,7 +59,8 @@ document.addEventListener('DOMContentLoaded', function () {
             'Ж': 'zh', 'З': 'z', 'И': 'i', 'Й': 'y', 'К': 'k', 'Л': 'l', 'М': 'm', 'Н': 'n',
             'О': 'o', 'П': 'p', 'Р': 'r', 'С': 's', 'Т': 't', 'У': 'u', 'Ф': 'f', 'Х': 'kh',
             'Ц': 'ts', 'Ч': 'ch', 'Ш': 'sh', 'Щ': 'shch', 'Ъ': '', 'Ы': 'y', 'Ь': '', 'Э': 'e',
-            'Ю': 'yu', 'Я': 'ya', 'ь': 'b', '.': '_', ':': '_', '?': '_', 'ъ': '_'
+            'Ю': 'yu', 'Я': 'ya', 'ь': 'b', '.': '_', ':': '_', '?': '_', 'ъ': '_', '[': '_',
+            ']': '_', '»':'_', '«': '_', '/': '_'
         };
         return word.split('').map(char => translitMap[char] || char).join('');
     }
